5eb467170efdb74dd62fbaf7bd01bc3b1113686e
Ozair0101 <ozairjan186@gmail.com>
2025-12-14 14:56:56 +0430

    a

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Home from './pages/Home'
 function App() {
   return (
     <div
-      className="relative flex min-h-screen w-full flex-col overflow-x-hidden bg-[#f8f7f5] text-[#333333] dark:bg-[#1a2533] dark:text-[#E0E0E0]"
+      className="relative flex min-h-screen w-full flex-col overflow-x-hidden bg-white text-gray-800"
       style={{ fontFamily: "Inter, ui-sans-serif, system-ui, -apple-system, Segoe UI, Roboto, Helvetica, Arial, 'Apple Color Emoji', 'Segoe UI Emoji'" }}
     >
       <Header />
