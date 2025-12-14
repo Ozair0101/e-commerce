a7126488f9589462259d0486f14d74719f95b0a6
Ozair0101 <ozairjan186@gmail.com>
2025-12-14 15:06:46 +0430

    c

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -9,18 +9,18 @@ export interface Category {
 const Categories: React.FC<{ categories: Category[] }> = ({ categories }) => {
   return (
     <section>
-      <h2 className="px-4 pb-3 pt-5 text-[22px] font-bold leading-tight tracking-[-0.015em]">Shop by Category</h2>
+      <h2 className="px-4 pb-3 pt-5 text-[22px] font-bold leading-tight tracking-[-0.015em] text-gray-800">Shop by Category</h2>
       <div className="grid grid-cols-2 gap-4 p-4 md:grid-cols-4">
         {categories.map((c) => (
           <div
             key={c.id}
-            className="flex flex-col items-center gap-3 rounded-lg border border-gray-200 bg-white p-4 pb-3 shadow-sm transition-shadow hover:shadow-lg dark:border-gray-700 dark:bg-gray-800"
+            className="flex flex-col items-center gap-3 rounded-lg border border-gray-200 bg-white p-4 pb-3 shadow-sm transition-shadow hover:shadow-lg"
           >
             <div
               className="h-40 w-full rounded-lg bg-cover bg-center"
               style={{ backgroundImage: `url(${c.image})` }}
             />
-            <p className="text-base font-medium leading-normal">{c.name}</p>
+            <p className="text-base font-medium leading-normal text-gray-800">{c.name}</p>
           </div>
         ))}
       </div>
