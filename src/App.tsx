4b0da6fbedac1c4ccb7fcfaf8584ba855e48b6c2
Ozair0101 <ozairjan186@gmail.com>
2025-12-14 15:24:27 +0430

    a

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,26 @@ import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './pages/Home'
+import About from './pages/About'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div
-      className="relative flex min-h-screen w-full flex-col overflow-x-hidden bg-white text-gray-800"
-      style={{ fontFamily: "Inter, ui-sans-serif, system-ui, -apple-system, Segoe UI, Roboto, Helvetica, Arial, 'Apple Color Emoji', 'Segoe UI Emoji'" }}
-    >
-      <Header />
-      <main className="flex w-full flex-1 flex-col items-center">
-        <Home />
-      </main>
-      <Footer />
-    </div>
+    <Router>
+      <div
+        className="relative flex min-h-screen w-full flex-col overflow-x-hidden bg-white text-gray-800"
+        style={{ fontFamily: "Inter, ui-sans-serif, system-ui, -apple-system, Segoe UI, Roboto, Helvetica, Arial, 'Apple Color Emoji', 'Segoe UI Emoji'" }}
+      >
+        <Header />
+        <main className="flex w-full flex-1 flex-col items-center">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   )
 }
 
