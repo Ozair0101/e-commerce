d551f18defef6ff8138fe5850390e327217d9354
Ozair0101 <ozairjan186@gmail.com>
2025-12-14 16:12:32 +0430

    a

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './pages/Home'
 import About from './pages/About'
+import Shop from './pages/Shop'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
+            <Route path="/shop" element={<Shop />} />
           </Routes>
         </main>
         <Footer />
